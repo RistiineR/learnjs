36a3faef7cc8b9ccc4069efd6850f7761c626cd6
RistiineR <ristiine.ruven@gmail.com>
2025-03-16 14:34:23 +0200

    Common js vs es module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import test from './test.js';
-test.hello();
+//import es from './es.js';
+const es = require('./es.js').default;
+
+es.hello();
+//import common from './common.js';
+console.log(common);
+const common = require('./common.js');
+common.hello();
